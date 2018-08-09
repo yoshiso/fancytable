18a944b31d0262d26c549eacaa2361d78cb5be68
yoshiso <nya060@gmail.com>
2018-08-08 21:17:09 -0700

    Bump v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fancytable',
-    version='0.0.1',
+    version='0.0.2',
     description='Display pd.DataFrame with fancy colors.',
     license='MIT',
     author='Sho Yoshida',
